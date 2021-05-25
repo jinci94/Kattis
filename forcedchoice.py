5c26e6550e2d6286b5d9541bb220176f9a35a00e
Jinci Brettschneider <31736593+jinci94@users.noreply.github.com>
2021-05-25 19:43:05 +0200

    Delete forcedchoice.py

--- forcedchoice.py
+++
@@ -1,8 +0,0 @@
-N, P, S = [int(x) for x in input().split()]
-
-for i in range(S):
-    x = [int(x) for x in input().split()]
-    if P in x[1:]:
-        print('KEEP')
-    else:
-        print('REMOVE')
